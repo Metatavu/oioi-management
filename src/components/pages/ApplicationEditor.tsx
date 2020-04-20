5b8ce60ad3603fb0eb521193b14c66f2fdef4f6f
Tero Lamminpaa <tero@pop-os.localdomain>
2020-04-20 10:58:06 +0300

    Commit before merge

--- src/components/pages/ApplicationEditor.tsx
+++ src/components/pages/ApplicationEditor.tsx
@@ -108,7 +108,7 @@ class ApplicationEditor extends React.Component<Props, State> {
 
   public componentDidUpdate = async (prevProps: Props, prevState: State) => {
     if (prevProps.resourceViewUpdated !== this.props.resourceViewUpdated) {
-      //await this.fetchResourceData();
+      await this.fetchResourceData();
     }
   };
 
