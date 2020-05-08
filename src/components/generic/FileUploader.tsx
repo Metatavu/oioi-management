cb40d1462d293ba3e506b64382daeac79f3f3cd8
Heikki Kurhinen <heikki.kurhinen@metatavu.fi>
2020-05-08 10:16:07 +0300

    Update src/components/generic/FileUploader.tsx
    Co-authored-by: Jari Nykänen <45241047+jarinykanen@users.noreply.github.com>

--- src/components/generic/FileUploader.tsx
+++ src/components/generic/FileUploader.tsx
@@ -172,7 +172,7 @@ class FileUploader extends React.Component<Props, State> {
             label={ strings.urlAddressDialogLabel }
             type="url"
             onChange={ this.handleResourceUrlChange }
-            value={this.state.resourceUrl || ""}
+            value={ this.state.resourceUrl || "" }
             fullWidth
           />
         </DialogContent>
