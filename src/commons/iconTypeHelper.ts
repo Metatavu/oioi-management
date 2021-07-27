b5309677da2c613564a2842214bc4fffc2a66468
Tuomas Kämppi <tuomas.kamppi@metatavu.fi>
2021-07-27 12:05:18 +0300

    fixed tsdocs

--- src/commons/iconTypeHelper.ts
+++ src/commons/iconTypeHelper.ts
@@ -34,7 +34,7 @@ export const getLocalizedIconTypeString = (type: IconKeys): string => ({
 })[type];
 
 /**
- * Get localized string for each icon key
+ * Get default icon url for each icon key
  * @param type icon key
  */
 export const getDefaultIconURL = (type: IconKeys): string => ({
