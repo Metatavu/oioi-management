60c1616a3ad9d11221181c69848a0415aef9ddc0
Lauri Lukkarinen <lauri.lukkarinen@metatavu.fi>
2021-08-12 08:56:52 +0300

    Added application layout

---
+++ src/styles/layouts/app-layout.ts
@@ -0,0 +1,20 @@
+import { makeStyles } from "@material-ui/core";
+import theme from "../theme";
+
+export const useEditorLayoutStyles = makeStyles({
+
+  root: {
+    backgroundColor: theme.palette.background.default,
+    height: "100vh",
+    width: "100vw",
+    display: "flex",
+    flexDirection: "column"
+  },
+
+  content: {
+    flex: 1
+  }
+
+}, {
+  name: "editor-layout"
+});
