88062b51b144c851ed47f215b260697c6fce9dbf
jarinykanen <jarinykanen.koodausta@gmail.com>
2021-09-17 15:21:39 +0300

    Added service worker for resource locks

---
+++ public/resource-lock-worker.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-restricted-globals */
+self.addEventListener("message", async (event) => {
+  const { url, method, headers, body } = event.data;
+
+  try {
+    await fetch(url, {
+      method: method,
+      headers: headers,
+      body: body
+    });
+  } catch (error) {
+    console.error("error", error);
+  }
+});
--- src/components/generic/ResourceTree.tsx
+++ src/components/generic/ResourceTree.tsx
@@ -18,6 +18,7 @@ import { toast } from "react-toastify";
 import { ResourceUtils } from "utils/resource";
 import AddResourceDialog from "./AddResourceDialog";
 import theme from "styles/theme";
+import { Config } from "app/config";
 
 /**
  * Component properties
@@ -117,7 +118,7 @@ class ResourceTree extends React.Component<Props, State> {
    * Component will unmount life cycle handler
    */
   public componentWillUnmount = async () => {
-    await this.componentCleanup();
+    this.componentCleanup();
     window.removeEventListener("beforeunload", this.componentCleanup);
   }
 
@@ -125,10 +126,34 @@ class ResourceTree extends React.Component<Props, State> {
    * Component clean up for unmount and beforeunload page event
    */
   private componentCleanup = async () => {
-    const { selectedResource } = this.props;
-
-    selectedResource && await this.releaseLock(selectedResource);
     this.resourceLocksInterval && clearInterval(this.resourceLocksInterval);
+    this.releaseLockWithServiceWorker();
+  }
+
+  /**
+   * Releases lock with service worker
+   */
+  private releaseLockWithServiceWorker = () => {
+    const { keycloak, customer, device, application, selectedResource } = this.props;
+
+    if (!keycloak?.token || !customer?.id || !device?.id || !application?.id || !selectedResource?.id) {
+      return;
+    }
+
+    navigator.serviceWorker.controller?.postMessage({
+      url: `${Config.get().api.baseUrl}/v1/customers/${customer.id}/devices/${device.id}/applications/${application.id}/resources/${selectedResource.id}/lock`,
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json;charset=UTF-8",
+        "Authorization": `Bearer ${keycloak.token}`
+      },
+      body: JSON.stringify({
+        customerId: customer.id,
+        deviceId: device.id,
+        applicationId: application.id,
+        resourceId: selectedResource.id
+      })
+    });
   }
 
   /**
@@ -258,7 +283,7 @@ class ResourceTree extends React.Component<Props, State> {
       customerId: customer.id,
       deviceId: device.id,
       applicationId: application.id,
-      resourceId: resource.id,
+      resourceId: resource.id
     });
   }
 
