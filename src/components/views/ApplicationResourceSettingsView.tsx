afaa76788d2ea500f5ea11ac3c8bb99476720140
Tuomas Kämppi <33017418+Kaemppi@users.noreply.github.com>
2021-10-01 16:56:14 +0300

    Update src/components/views/ApplicationResourceSettingsView.tsx
    Co-authored-by: Lauri Lukkarinen <44664737+lauriLukkarinen@users.noreply.github.com>

--- src/components/views/ApplicationResourceSettingsView.tsx
+++ src/components/views/ApplicationResourceSettingsView.tsx
@@ -699,7 +699,7 @@ class ApplicationResourceSettingsView extends React.Component<Props, State> {
    * Toggle dialog
    */
   private toggleDialog = () => {
-    this.setState({iconDialogOpen: !this.state.iconDialogOpen});
+    this.setState({ iconDialogOpen: !this.state.iconDialogOpen });
   }
 
   /**
