f5d3cde35c0d3fc1756eaa06a54208c08a1ed4a7
Tuomas Kämppi <33017418+Kaemppi@users.noreply.github.com>
2021-10-01 16:12:05 +0300

    Update src/components/views/LanguageResourceSettingsView.tsx
    Co-authored-by: Lauri Lukkarinen <44664737+lauriLukkarinen@users.noreply.github.com>

--- src/components/views/LanguageResourceSettingsView.tsx
+++ src/components/views/LanguageResourceSettingsView.tsx
@@ -699,7 +699,7 @@ class LanguageResourceSettingsView extends React.Component<Props, State> {
    * Toggle dialog
    */
   private toggleDialog = () => {
-    this.setState({iconDialogOpen: !this.state.iconDialogOpen});
+    this.setState({ iconDialogOpen: !this.state.iconDialogOpen });
   }
 
   /**
