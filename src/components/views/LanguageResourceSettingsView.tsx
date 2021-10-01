81807c91da5b9753890f9cb2292d5ea6d78b0ad1
Tuomas Kämppi <33017418+Kaemppi@users.noreply.github.com>
2021-10-01 16:12:16 +0300

    Update src/components/views/LanguageResourceSettingsView.tsx
    Co-authored-by: Lauri Lukkarinen <44664737+lauriLukkarinen@users.noreply.github.com>

--- src/components/views/LanguageResourceSettingsView.tsx
+++ src/components/views/LanguageResourceSettingsView.tsx
@@ -733,7 +733,7 @@ class LanguageResourceSettingsView extends React.Component<Props, State> {
     }
 
     onUpdate({
-      ...this.props.resource,
+      ...resource,
       name: name,
       orderNumber: orderNumber,
       slug: slug,
